00f36d47ca19c261cb018d8e260470d30f98df9f
1999Snehal <snehallagad84@gmail.com>
2024-10-26 11:40:26 +0530

    adding files

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+class LL
+{
+    public static void main(String arg[])
+    {
+        LinkedList<String> animals = new LinkedList<String>();
+
+        animals.add("Cat");
+        animals.add("Dog");
+        animals.add("Cow");
+
+        System.out.println("List of Animals :"+animals);
+
+        animals.add(1,"Horse");
+        System.out.println("List of Animals :"+animals);
+
+        String str = animals.get(2);
+        System.out.println("Animal of index 2 is :"+str);
+
+        animals.set(3,"Bull");
+        System.out.println("Updated List of animal is :"+animals);
+
+        animals.addFirst("Elephant");
+        animals.addLast("Zebra");
+        System.out.println("Updated List of animal is :"+animals);
+
+        animals.removeLast();
+        System.out.println("Updated List of animal is :"+animals);
+
+
+
+
     }
 }
